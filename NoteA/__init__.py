d54aed54fa8ad464e2aa8e81fccf95ccb91893db
Void-Stag <64465757+Void-Stag@users.noreply.github.com>
2023-10-23 20:51:42 +0100

    database creation

--- NoteA/__init__.py
+++ NoteA/__init__.py
@@ -1,12 +1,28 @@
-import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from .routes import main
-#from .auth import routes.auth
+db = SQLAlchemy()
 
 
-NoteA = Flask(__name__)
-NoteA.debug = True
+def create_app():
+    NoteA = Flask(__name__)
+    NoteA.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///website.db'
 
-#Importing blueprints
-NoteA.register_blueprint(main)
-#NoteA.register_blueprint(routes.auth)
+    db.init_app(NoteA)
+    NoteA.register_blueprint(main)
+
+    with NoteA.app_context():
+        db.create_all()
+    return NoteA
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    taskname = db.Column(db.String(75))
+    duedate = db.Column(db.DateTime)
+
+    def __init__(self, taskname, duedate):
+        self.taskname = taskname
+        self.duedate = duedate
+
+    def __repr__(self):
+        return f'<Task {self.taskname}>'
