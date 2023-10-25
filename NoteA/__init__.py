7415c0a212134404c4ded408acc950360a403018
Void-Stag <64465757+Void-Stag@users.noreply.github.com>
2023-10-25 14:36:01 +0100

    flask-migrate setup

--- NoteA/__init__.py
+++ NoteA/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from NoteA import routes, authroutes
-db = SQLAlchemy()
+from flask_migrate import Migrate
 
+NoteA = Flask(__name__)
+NoteA.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///website.db'
+
+db = SQLAlchemy(NoteA)
+migrate = Migrate(NoteA, db)
 
 def create_app():
     NoteA = Flask(__name__)
