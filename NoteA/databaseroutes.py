912f4bfaaeef4f3bc7837baa19369bf7635c0155
Void-Stag <64465757+Void-Stag@users.noreply.github.com>
2023-10-29 18:15:07 +0000

    full basic task implementation

--- NoteA/databaseroutes.py
+++ NoteA/databaseroutes.py
@@ -2,25 +2,27 @@ from flask import Blueprint, redirect, request
 from run import db
 from models import Task
 from flask import jsonify
-
+from datetime import datetime
 data = Blueprint('data', __name__, template_folder='templates')
 
 #ToDo Tasks
 @data.route('/addtask', methods=['POST'])
 def addtask():
-    task = request.form.get('taskname')
-    date = request.form.get('duedate')
-
-    if task!= '' and date !='' !=None:
-        t = Task(taskname=task, duedate=date)
+    taskname = request.form.get('taskname')
+    duedate = datetime.strptime(request.form.get('duedate'), '%d/%m/%Y')
+    print(f"{request.form}")
+    print(f"addtask 1: {taskname} {duedate}")
+    if taskname!= '' and duedate !='' !=None:
+        t = Task(taskname=taskname, duedate=duedate)
         db.session.add(t)
         db.session.commit()
 
-    redirect('/home')
+    return redirect('/home')
 
 @data.route('/showtask', methods=['GET','POST'])
 def showtask():
     task = []
     for row in Task.query.all():
-        task.append((str(row.task), str(row.date)))
+        task.append((str(row.taskname), str(row.duedate)))
+        print(f"showtask {task}")
     return jsonify(task)
