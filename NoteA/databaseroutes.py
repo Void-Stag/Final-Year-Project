81a5d36bdb05f6c64b247e3c93c3449d3ff477a4
Void-Stag <64465757+Void-Stag@users.noreply.github.com>
2023-11-06 14:43:44 +0000

    display fix

--- NoteA/databaseroutes.py
+++ NoteA/databaseroutes.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, redirect, request
 from run import db
 from models import Task
-from flask import jsonify
 from datetime import datetime
 data = Blueprint('data', __name__, template_folder='templates')
 
@@ -10,9 +9,7 @@ data = Blueprint('data', __name__, template_folder='templates')
 @data.route('/addtask', methods=['POST'])
 def addtask():
     taskname = request.form.get('taskname')
-    duedate = datetime.strptime(request.form.get('duedate'), '%d/%m/%Y')
-    #print(f"{request.form}")
-    #print(f"addtask 1: {taskname} {duedate}")
+    duedate = datetime.fromisoformat(request.form.get('duedate'))
     if taskname!= '' and duedate !='' !=None:
         t = Task(taskname=taskname, duedate=duedate)
         db.session.add(t)
@@ -20,23 +17,11 @@ def addtask():
 
     return redirect('/home')
 
-@data.route('/showtask', methods=['GET','POST'])
-def showtask():
-    task = []
-    for row in Task.query.all():
-        task.append((str(row.taskname), str(row.duedate)))
-        #print(f"showtask {task}")
-    js = jsonify(task)
-    print(f"show_task {js}")
-
-    return js
-
-@data.route('/delete_task/<int:id>')
+@data.route('/delete_task/<int:id>', methods=['GET', 'POST'])
 def delete_task(id):
     if id:
         try:
             Task.query.filter(Task.id == id).delete()
-            print(f"delete_task {id}")
             db.session.commit()
         except Exception as error:
             print(f"delete_task [{id}] Fail {error}")
