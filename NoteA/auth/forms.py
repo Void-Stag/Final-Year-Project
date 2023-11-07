32e1c5186eb5668a7c067fcbee5face97b05754f
Void-Stag <64465757+Void-Stag@users.noreply.github.com>
2023-11-07 17:10:44 +0000

    WTForms Formatting

--- NoteA/auth/forms.py
+++ NoteA/auth/forms.py
@@ -1,21 +1,21 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length, NumberRange
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 from models import User
 
 
 class UserLogin(FlaskForm):
-    Username = StringField('Username', validators=[DataRequired()])
-    Password = PasswordField('Password', validators=[DataRequired()])
+    Username = StringField('', validators=[DataRequired()])
+    Password = PasswordField('', validators=[DataRequired()])
     Login = SubmitField('Login')
 
 
 class CreateAccount(FlaskForm):
-    Username = StringField('Username', validators=[DataRequired()])
-    Password = PasswordField('Password', validators=[DataRequired()])
-    PasswordRetype = PasswordField('PasswordRetype', validators=[DataRequired(), EqualTo('Password', 'Passwords must match')])
-    CreateAccount = SubmitField('CreateAccount')
+    Username = StringField('', validators=[DataRequired()])
+    Password = PasswordField('', validators=[DataRequired(), Length(min=8, max=20)])
+    PasswordRetype = PasswordField('', validators=[DataRequired(), EqualTo('Password', 'Passwords must match')])
+    CreateAccount = SubmitField('Create Account')
 
     def validate_username(self, Username):
         user = User.query.filter_by(username=Username.data).first()
--- NoteA/routes.py
+++ NoteA/routes.py
@@ -4,8 +4,11 @@ from models import Task
 web = Blueprint('web', __name__, template_folder='templates')
 
 
-@web.route('/home', methods=['GET','POST'])
-def home(cat=None):
+@web.route('/Home', methods=['GET','POST'])
+def home():
     tasks = Task.query.all()
     return render_template('homepage.html', title='Home' ,tasks=tasks)
 
+@web.route('/AccountSettings', methods=['GET', 'POST'])
+def AccountSettings():
+    return render_template('Account/settings.html', title='Settings')
